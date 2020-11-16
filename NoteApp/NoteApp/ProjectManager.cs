078624f9a59fb307942ff7b30ebc4aeda95c338c
Андрей Асмоловский <dronee44@gmail.com>
2020-11-16 22:56:53 +0700

    Revert "Добавлен отчет к 1 ЛР"
    This reverts commit c3522183c4834f5d98899c542966624e10d853ea.

--- NoteApp/NoteApp/ProjectManager.cs
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -6,12 +6,12 @@ using System.Text;
 
 namespace NoteApp
 {
-        public class ProjectManager
+    class ProjectManager
     {
         /// <summary>
         /// константа содержащая путь
         /// </summary>
-        private static string _path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\NoteApp.json"; 
+        private static string _path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\NoteApp.notes"; 
         public static void SaveToFile(Project data, string file)
         {
             JsonSerializer serializer = new JsonSerializer
